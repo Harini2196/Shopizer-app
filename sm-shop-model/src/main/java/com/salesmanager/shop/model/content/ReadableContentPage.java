7fc53ff3d520f1bea7a0f8bb5a5257f02028568e
harini2196 <keesarinavya14@gmail.com>
2021-02-25 00:36:51 +0530

    first version

---
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/content/ReadableContentPage.java
@@ -0,0 +1,14 @@
+package com.salesmanager.shop.model.content;
+
+@Deprecated
+public class ReadableContentPage extends ReadableContentObject {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+
+}
